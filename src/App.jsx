c20c193669add634e9ef3664f513628a93924343
ulugb3kjon <ulugbeknurmatov940@gmail.com>
2025-09-13 12:35:06 +0500

    haasdasdasddl

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/main-layout";
 import Categories from "./pages/categories";
 import CategoryDetails from "./pages/category-details";
+import './index.css'; 
+
 
 function App() {
   return (
